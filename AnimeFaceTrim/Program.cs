280c84464273e413752755c95883a8228a5c2b30
ruhiel_murrue <ruhiel1983@gmail.com>
2016-11-13 23:09:02 +0900

    初回コミット

--- AnimeFaceTrim/Program.cs
+++ AnimeFaceTrim/Program.cs
@@ -1,8 +1,8 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using OpenCvSharp;
+using System.IO;
+using Mono.Options;
+
 
 namespace AnimeFaceTrim
 {
@@ -10,6 +10,84 @@ namespace AnimeFaceTrim
 	{
 		static void Main(string[] args)
 		{
+			try
+			{
+				string filepath = null;
+				var p = new OptionSet()
+					.Add("f|file=", "File Path", v => filepath = v);
+
+				p.Parse(args);
+
+				if (filepath == null)
+				{
+					Console.Error.WriteLine("Usage:AnimeFaceTrim [OPTIONS]");
+					Console.Error.WriteLine();
+					p.WriteOptionDescriptions(Console.Error);
+					return;
+				}
+
+				Mat image = Cv2.ImRead(filepath);
+
+				string outfile = Path.Combine(Path.GetDirectoryName(filepath), Path.GetFileNameWithoutExtension(filepath) + "_trim" + Path.GetExtension(filepath));
+
+				string xml = @"xml\lbpcascade_animeface.xml";
+				Mat detectFaceImage = DetectFaceInImage(image, Path.Combine(Directory.GetCurrentDirectory(), xml));
+				detectFaceImage.SaveImage(outfile);
+
+				Console.WriteLine("Detect Face Success. " + outfile);
+			}
+			catch(Exception)
+			{
+				Console.WriteLine("Detect Face Fail");
+			}
+			finally
+			{
+
+			}
+		}
+
+		static Mat DetectFaceInImage(Mat image, string cascade_file)
+		{
+			CascadeClassifier cascade = new CascadeClassifier();
+			cascade.Load(cascade_file);
+			Rect[] faces = cascade.DetectMultiScale(image, 1.1, 3, 0, new Size(20, 20));
+
+			var facesPointer = 0;
+
+			//　横位置取得
+			int nx = faces[facesPointer].X;
+
+			//　検出幅を1/3にして、横方向切り出し位置の基準とする。
+			int wbase = faces[facesPointer].Width;
+
+			//　縦位置取得
+			int ny = faces[facesPointer].Y;
+
+			//　縦方向切り出し位置の基準とする。
+			int hbase = faces[facesPointer].Height;
+
+			//　切り出し幅を幅基準の５倍とする。
+			int newWidth = (int)(wbase);
+
+			//　切り出し高さ幅基準
+			int newHeight = (int)(wbase);
+
+			//　切り出し位置を作成
+			int colEnd = nx + newWidth;
+			int rowEnd = ny + newHeight;
+
+			// 計算位置が元写真より大きい場合は、元写真の大きさに修正
+			if (colEnd > image.Width)
+			{
+				colEnd = image.Width;
+			}
+			if (rowEnd > image.Height)
+			{
+				rowEnd = image.Height;
+			}
+
+			//　元写真から検知部分を切り抜き
+			return image[ny, rowEnd, nx, colEnd];
 		}
 	}
 }
